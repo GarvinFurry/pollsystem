78761e8b18741492b9bd5c7100242f3a85dee719
Garvin <115864855+GarvinFurry@users.noreply.github.com>
2025-09-04 11:55:40 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,40 @@
+
+<br><br><br><br>
+<footer id="myFooter" class="mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h2 class="logo"><a href="index.php"><img src='img/200.png'></a></h2>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Get started</h5>
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="team.php">The Team</a></li>
+                        <li><a href="forum.php">The ADUN Forum</a></li>
+                        <li><a href="hub.php">The ADUN Hub</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>About us</h5>
+                    <ul>
+                        <li><a href="ADUN creators/Garvin.php">Animam Garvin Chinedu</a></li>
+
+                    </ul>
+                </div>
+                <div class="col-sm-2">
+                    <h5>Support</h5>
+                    <ul>
+                        <li><a href="Contactus/index.php">Contact Us</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    
+                    <a class="btn btn-default" href="Contactus/index.php">Contact us</a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All Rights Reserved </p>
+        </div>
+    </footer>
