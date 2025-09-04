467dafc940f3a393bc084d5074bfc966f2ceeff3
Garvin <115864855+GarvinFurry@users.noreply.github.com>
2025-09-04 11:35:41 +0100

    Add files via upload

---
+++ ADUN creators/Garvin.php
@@ -0,0 +1,220 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>ADUN Creator</title>
+        
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+        
+	<link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet"> 
+        
+	<link href="../css/creator-styles.css" rel="stylesheet">
+	
+	<link href="../css/creator-responsive.css" rel="stylesheet">
+        
+        <link href="../css/creator-portfolio.min.css" rel="stylesheet">
+        
+        <link href="../css/footer.css" rel="stylesheet">
+        
+        <link rel="shortcut icon" href="../img/favicon.png" />
+	
+</head>
+<body>
+	
+	<header>
+		<div class="container">
+			<div class="heading-wrapper">
+				<div class="row">
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-location-outline"></i>
+							<div class="right-area">
+								<h5>ADUN</h5>
+								<h5>EDUCTING LUMINARY LEADERS</h5>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+					
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-telephone-outline"></i>
+							<div class="right-area">
+								<h5>+2348105562699</h5>
+                 <h6>Official Number</h6>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+					
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-chatboxes-outline"></i>
+							<div class="right-area">
+								<h5>animamgarvin@gmail.com</h5>
+								<h6>Official Email</h6>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+				</div><!-- row -->
+			</div><!-- heading-wrapper -->
+			
+		</div><!-- container -->
+	</header>
+	
+    <section class="intro-section" style="background-image: url(img/anas-imran-cover.jpg);">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-1 col-lg-2"></div>
+				<div class="col-md-10 col-lg-8">
+					<div class="intro">
+                                            <div class="profile-img"><img src="img/anas-imran.JPG" alt=""></div>
+                                            <h2><b>Animam Garvin Chinedu</b></h2><br>
+                                            
+                                            <h4 class="font-yellow">What goes up, must come down</h4>
+                                                <b class="text-muted">(heard this outside his toilet cubicle)</b>
+                                                
+                                            <br><br>
+						<ul class="social-icons">
+							<li><a href="https://github.com/GarvinFurry" target="_blank">
+                                                                <i class="fa fa-github"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/Garvin Animam/" target="_blank">
+                                                                <i class="fa fa-linkedin"></i></a></li>
+							<li><a href="https://web.facebook.com/Garvin Walker" target="_blank">
+                                                                <i class="fa fa-facebook"></i></a></li>
+						</ul>
+					</div><!-- intro -->
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- intro-section -->
+
+        <br><br><br>
+	<section class="accordian-section section">
+		<div class="container">
+		
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="heading">
+						<h4><b>Skills</b></h4>
+					</div>
+				</div><!-- col-sm-12 -->
+				
+				<div class="col-md-12 col-lg-6">
+				
+					<div class="line-progress margin-b-20" data-prog-percent="1.00"><div></div>
+                                            <p class="progress-title"><b>Being Tech Savvy</b></p></div><br>
+					
+					<div class="line-progress margin-b-20" data-prog-percent=".85"><div></div>
+                                            <p class="progress-title"><b>Gaming</b></p></div><br>
+
+					<div class="line-progress margin-b-20" data-prog-percent=".70"><div></div>
+                                            <p class="progress-title"><b>Reading Books</b></p></div><br>
+					
+					<div class="line-progress margin-b-50" data-prog-percent=".90"><div></div>
+                                            <p class="progress-title"><b>Sleeping</b></p></div><br>
+
+				</div><!-- col-sm-6 -->
+				
+				<div class="col-md-12 col-lg-6">
+					<div class="panel margin-b-30">
+						<div class="panel-area margin-b-30">
+							<h5 class="panel-title active"><b>Passing Bad Puns</b>
+								<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+                                                                <p class="panel-desc">Why did the girl throw the clock out the window?<br>
+                                                                    She wanted to see the time fly!<br><br>
+                                                                (cricket sounds in the background)...</p>
+						</div><!-- panel-area -->
+						
+						<div class="panel-area margin-b-30">
+							<h5 class="panel-title"><b>Web Development</b>
+								<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+							<p class="panel-desc">I have specialized in advanced front/backend features
+                                                        of WordPress, as well as PHP, JS, JQuery, MYSQLI</p>
+						</div><!-- panel-area -->
+						
+						<div class="panel-area margin-b-30">
+							<h5 class="panel-title"><b>Machine Learning</b>
+								<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+							<p class="panel-desc">Currently an aspiring Computer Science student,
+                                                        with improved skills in ML and AI alongside with android and web development</p>
+						</div><!-- panel-area -->
+					</div><!-- panel -->
+				</div><!-- col-sm-6-->
+				
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- accordian-section -->
+	
+	
+        
+        
+        <section class="loaders-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="heading">
+						<h4><b>Aptitudes</b></h4>
+					</div>
+				</div><!-- col-sm-12 -->
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="radial-prog-area margin-b-30">
+						<div class="radial-progress" data-prog-percent=".05">
+							<div></div>
+							<h6 class="progress-title">Good Joker</h6>
+						</div>
+					</div><!-- radial-prog-area-->
+				</div><!-- col-sm-6-->
+				
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="radial-prog-area margin-b-30">
+						<div class="radial-progress" data-prog-percent=".65">
+							<div></div>
+							<h6 class="progress-title">Adaptability</h6>
+						</div>
+					</div><!-- radial-prog-area-->
+				</div><!-- col-sm-6-->
+				
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="radial-prog-area margin-b-30">
+						<div class="radial-progress" data-prog-percent=".85">
+							<div></div>
+							<h6 class="progress-title">Earnest Learning</h6>
+						</div>
+					</div><!-- radial-prog-area-->
+				</div><!-- col-sm-6-->
+				
+				<div class="col-sm-6 col-md-6 col-lg-3">
+					<div class="radial-prog-area margin-b-50">
+						<div class="radial-progress" data-prog-percent=".97">
+							<div></div>
+                                                        <h6 class="progress-title">HISSSSSSS</h6>
+						</div>
+					</div><!-- radial-prog-area-->
+				</div><!-- col-sm-6-->
+				
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- loaders-section -->
+        
+        
+    
+              </div>
+            </div>
+          </div>
+        </section>
+        
+	
+	<?php include 'assets/footer.php'; ?>
+	
+        
+	
+	<script src="../js/jquery.min.js"></script>
+	
+	<script src="../js/bootstrap.min.js"></script>
+	
+	<script src="../js/jquery.waypoints.min.js"></script>
+	
+	<script src="../js/progressbar.min.js"></script>
+	
+	<script src="../js/scripts.js"></script>
+	
+</body>
+</html>
